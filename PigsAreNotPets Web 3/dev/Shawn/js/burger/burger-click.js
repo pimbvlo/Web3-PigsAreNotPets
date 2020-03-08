69bcfa4e88e01e948220e18e6619372ff2dbe815
pimbvlo <38928988+pimbvlo@users.noreply.github.com>
2020-03-08 14:57:56 -0400

    Finished final sections
    Fixed header and footer problems I think?

--- PigsAreNotPets Web 3/dev/Shawn/js/burger/burger-click.js
+++ PigsAreNotPets Web 3/dev/Shawn/js/burger/burger-click.js
@@ -12,8 +12,8 @@ lineToLines.to("#top-line",{duration:burgerSpeed, y:0},"move-lines")
 
 
 var staggerNav = gsap.timeline({paused:true});
-staggerNav.to("#nav-bg", {duration: burgerSpeed+(burgerSpeed/8), scale: 35, alpha: 0.98})
-.to("#nav-bg", {duration: burgerSpeed, backgroundColor:"#FF5D00", alpha: 1}, "-=burgerSpeed")
+staggerNav.to("#nav-bg", {duration: burgerSpeed+(burgerSpeed/8), scale: 40, alpha: 0.95})
+.to("#nav-bg svg circle", {duration: burgerSpeed, alpha: 0.95}, "-=burgerSpeed")
 .to("nav ul li", {duration:burgerSpeed/4, stagger:0.1, alpha:1, y: 10, display:"initial"}, "-=0.15");
 
 $('#burger-container').on("click", burgerClicked);
--- PigsAreNotPets Web 3/dev/Shawn/js/navigation.js
+++ PigsAreNotPets Web 3/dev/Shawn/js/navigation.js
@@ -5,7 +5,7 @@
 // when a link is clicked, force the menu to reverse all the animations
 $('nav ul li').on("click", burgerClicked);
 
-var contentSections = ["#section-1", "#section-2", "#section-3", "#section-4"];
+// var contentSections = ["#section-1", "#section-2", "#section-3", "#section-4"];
 // var st = $('#section-3').scrollTop();
 // console.log(st);
 // listen for the window to resize and reset the nav width var
