db78e452e23e8b59fce3eb619a7a06d9aa3e7e3d
pimbvlo <38928988+pimbvlo@users.noreply.github.com>
2020-03-08 18:59:51 -0400

    HTML/KIT files are done

--- PigsAreNotPets Web 3/dev/Shared/js/scripts.js
+++ PigsAreNotPets Web 3/dev/Shared/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Shared");
+// @codekit-prepend "navigation.js";
+
+// @codekit-prepend "burger/burger.js";
+// @codekit-prepend "burger/burger-mouseenter.js";
+// @codekit-prepend "burger/burger-mouseleave.js";
+// @codekit-prepend "burger/burger-click.js";
+
+// @codekit-prepend "waypoints.js";
+
+
+
+$(document).ready(function() {
+    gsap.registerPlugin(ScrollToPlugin);
+
+    console.log("ready!");
+});
