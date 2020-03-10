ad9e476670f9c395210af9bd44abbb3914d95add
Matt <mbb670@gmail.com>
2020-03-09 20:41:00 -0400

    many updates

---
+++ PigsAreNotPets Web 3/dev/Shared/js/fullpage.js
@@ -0,0 +1,28 @@
+new fullpage('#fullPage', {
+
+    // page navigation
+    // autoScrolling:true,
+    // navigation:true,
+    // scrollingSpeed:1000,
+
+    // slide navigation
+    controlArrows:true,
+    slidesNavigation:true,
+    // loopHorizontal:true,
+
+    // a fixed element over every screen
+    // fixedElements: '#element1',
+
+    // Continuous Vertical scrolling
+    // continuousVertical:true,
+
+    // showing tooltips on nav circles
+    // navigationTooltips:['INTRO', 'SLIDES', 'FEATURES'],
+    // showActiveTooltip:true,
+
+    // pages which go beyond the viewport height
+    scrollOverflow:true,
+    scrollOverflowReset:true,
+    fitToSection: false,
+    autoScrolling:false
+});
--- PigsAreNotPets Web 3/dev/Shared/js/scripts.js
+++ PigsAreNotPets Web 3/dev/Shared/js/scripts.js
@@ -4,6 +4,7 @@
 // @codekit-prepend "burger/burger-mouseenter.js";
 // @codekit-prepend "burger/burger-mouseleave.js";
 // @codekit-prepend "burger/burger-click.js";
+// @codekit-prepend "fullPage.js";
 
 // @codekit-prepend "waypoints.js";
 
